df9400e0b857d031410467d4e9bd738e81b4b15f
Ishan-Karpe <ishan.k.abhijeet@gmail.com>
2025-02-15 13:53:14 -0800

    gets input from user

--- translate.py
+++ translate.py
@@ -1,11 +1,11 @@
 import googletrans, asyncio
 
 
-async def main():
+async def main(input_text):
     # lang = googletrans.LANGUAGES
     # print(lang) # there are a LOT languages (over 100), organized by their language code
     trans = googletrans.Translator() # create a translator object
-    translated=await trans.translate('Hola') # dectected language to english, or specifiy dest() to translate to a specific language
+    translated=await trans.translate(input_text) # dectected language to english, or specifiy dest() to translate to a specific language
     print(translated.text) # print the translated text
 
 
@@ -18,7 +18,10 @@ async def main():
     translated = await trans.translate(texts, dest='mr', src=dt1.lang) # translate to Marathi
     print(translated.text)
 
-asyncio.run(main())
+    translated = await trans.translate(input_text, dest='es', src=dt1.lang) # translate to Marathi
+    print(translated.text)
+
+asyncio.run(main("to be or not to be"))
 
 # async is used to make the function asynchronous, so it can run in parallel with other functions
 # await is used to wait for the function to finish, so the program doesn't continue until the function is done
